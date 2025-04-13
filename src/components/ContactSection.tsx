aaa4b1f87b43a773ab468a4517fe57207dcf5074
ahsanbq <ahsan@konnectingdots.com>
2025-04-13 11:33:53 +0600

    map fix

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -265,7 +265,7 @@ const ContactSection = () => {
           className="mt-16 h-[400px] rounded-xl overflow-hidden shadow-lg"
         >
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3650.511731473569!2d90.35276647531052!3d23.80039548685522!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c135b300e601%3A0x4e91dba279841654!2sSony%20Cinema%20Hall!5e0!3m2!1sen!2sbd!4v1744471011020!5m2!1sen!2sbd"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3650.4570939470673!2d90.35041109398553!3d23.802339703918207!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c1b49e00fa85%3A0x46e4c5a45bfe5d5f!2sAzure%20Cuisine!5e0!3m2!1sen!2sbd!4v1744522331746!5m2!1sen!2sbd"
             width="100%"
             height="100%"
             style={{ border: 0 }}
